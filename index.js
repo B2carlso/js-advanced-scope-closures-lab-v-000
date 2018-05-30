a1a720a44333096ae6311dfae26b1320ad4e1446
Brad Carlson <b2carlso@yahoo.com>
2018-05-30 21:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock) {
+    start = parseInt(startBlock);
+    end = parseInt(endBlock);
+    distance = Math.abs(end - start);
+    range = blockRange - distance;
 
+    if (range <0) {
+      return `${Math.abs(range)} blocks out of range`;
+    }
   }
 }
