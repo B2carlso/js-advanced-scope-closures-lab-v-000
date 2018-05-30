70b6b003929dfc733d2ae3ba1a0eace829d582ed
Brad Carlson <b2carlso@yahoo.com>
2018-05-30 21:45:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function produceDrivingRange(blockRange) {
+  return function(startBlock, endBlock) {
+    start = parseInt(startBlock);
+    end = parseInt(endBlock);
+    distance = Math.abs(end - start);
+    range = blockRange - distance;
+
+    if (range <0) {
+      return `${Math.abs(range)} blocks out of range`;
+    } else {
+      return `within range by ${range}`;
+    }
+  }
+}
+
+
+function produceTipCalculator(tipAmount) {
+  return function(ridePrice) {
+    return ridePrice * tipAmount;
+  }
+}
